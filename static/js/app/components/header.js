c62aa409f6c36fc550687b0e3c7764dda92837f7
Ubuntu <ubuntu@ip-10-0-0-131.us-west-2.compute.internal>
2015-02-09 01:29:17 +0000

    lots of tweaks

--- static/js/app/components/header.js
+++ static/js/app/components/header.js
@@ -5,7 +5,15 @@ var Header = React.createClass({
     render: function() {
         return (
             <div className="header">
-                <a className="rewrite logo" href="">Debate-a-bot</a>
+                <div className="header-share">
+                    <iframe src="//www.facebook.com/plugins/share_button.php?href=http%3A%2F%2Fdebateabot.com&amp;layout=button_count&amp;appId=211879852169399" scrolling="no" frameborder="0" style={{ border: "none", overflow: "hidden" }} allowTransparency="true"></iframe>
+                </div>
+                <div className="header-share">
+                    <a href="https://twitter.com/share" className="twitter-share-button" data-text="I just talked to bots!" data-hashtags="comedyhackday,debate-a-bot">Tweet</a>
+                </div>
+                <a className="rewrite logo" href="">
+                    <img src="static/images/header_logo.png" />
+                </a>
             </div>
         );
     }
